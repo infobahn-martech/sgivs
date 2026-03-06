2ca29d2113547aaadabe241ad55978c97d490e23
Dany Thomas <prog3@infobahnworld.com>
2026-03-06 11:06:48 +0400

    updates

--- src/services/inScanService.js
+++ src/services/inScanService.js
@@ -1,6 +1,6 @@
 import Gateway from '../config/gateway';
 
-const getData = (params) => Gateway.post('/passport/list', { ...params, status: 3 });
+const getData = () => Gateway.post('passport/status_list', { status: 18 });
 
 const bulkStatusChange = ({ status_id, employee_id, application_numbers }) =>
     Gateway.post('/passport_scan/bulk_status_change', {
